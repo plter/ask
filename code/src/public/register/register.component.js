055c47c7df766694b4b94875d197dea859a6a633
梦宇 <xtiqin@163.com>
2016-11-24 11:56:14 +0800

    实现注册页面

--- code/src/public/register/register.component.js
+++ code/src/public/register/register.component.js
@@ -3,5 +3,29 @@
  */
 
 angular.module("register").component("register", {
-    templateUrl: "register/register.template.htm"
+    templateUrl: "register/register.template.htm",
+    controller: function ($scope) {
+        $scope.login = "";
+        $scope.pass = "";
+        $scope.passConfirm = "";
+
+        var alertDiv = $(".register-form-alert");
+        alertDiv.hide();
+        alertDiv.find(".close").click(function () {
+            alertDiv.hide("fast");
+        });
+        var alertContent = alertDiv.find(".alert-content");
+
+        $scope.formSubmitHandler = function () {
+
+            if ($scope.pass != $scope.passConfirm) {
+                alertContent.html("密码确认不一致");
+                alertDiv.show("fast");
+                return;
+            }
+            alertDiv.hide("fast");
+
+            //TODO connect server to perform the register action
+        }
+    }
 });
